1f17cbf17e7c24e6f70f1eb83791761ddfac80df
Stormy <80485413+0hStormy@users.noreply.github.com>
2025-01-26 19:32:32 -0800

    Oh my, it might have changed a bit

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ def readFile(file):
     with open(file, "r") as f:
         return f.read()
 
-def renderChat():
+def renderChat(name=""):
     template = readFile("static/index.html")
     with open("internal/chathistory", "r") as f:
         rawHistory = f.readlines()
+        rawHistory.reverse()
     formattedChat = ""
     for message in rawHistory:
         splitMessage = message.split("[@spl]")
-        formattedChat = formattedChat + f"<p><p class='nameTag'>{splitMessage[0]}: </p>{splitMessage[1]}</p>\n"
+        formattedChat = formattedChat + f"<div><p><a class='nameTag'>{splitMessage[0]}: </a>{splitMessage[1]}</p></div>\n"
     formattedChat.removesuffix("\n")
     returnFile = template.replace("$chat", formattedChat)
+    returnFile = returnFile.replace("$name", name)
     return returnFile
 
 def addMessage(message, name):
@@ -25,12 +27,13 @@ app = Flask(__name__)
 @app.route("/", methods=('GET', 'POST'))
 def appGrid():
     if request.method == 'POST':
-        if request.form["message"] == "":
-            pass
-        if request.form["username"] == "":
-            pass
+        if request.form["username"] == "" or None:
+            return renderChat()
+        if request.form["message"] == "" or None:
+            print(repr(request.form["message"]))
+            return renderChat(request.form["username"])
         else:
             addMessage(request.form["message"], request.form["username"])
-        return renderChat()
+        return renderChat(request.form["username"])
     else:
         return renderChat()
