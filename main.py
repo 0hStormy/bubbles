5558531edada48e75e79518273bd329eff558312
Stormy <80485413+0hStormy@users.noreply.github.com>
2025-01-28 16:16:20 -0800

    I MIGHT have added emojis (thanks Microsoft for the art)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import time
+import os
 
 def readFile(file):
     with open(file, "r") as f:
@@ -19,8 +20,17 @@ def renderChat(name=""):
     returnFile = returnFile.replace("$name", name)
     return returnFile
 
+def emotes(message):
+    start_pt = message.find(":")
+    end_pt = message.find(":", start_pt + 1)
+    emoji = message[start_pt: end_pt + 1]
+    emoji = emoji.replace(":", "")
+    returnText = message.replace(f":{emoji}:", f'<img width="16px" src="static/emotes/face-{emoji}.png">')
+    return returnText
+
 def addMessage(message, name):
     with open("internal/chathistory", "a") as f:
+        message = emotes(message)
         postTime = time.strftime('%l:%M%p %Z, %b %d, %Y')
         f.write(f"{name}[@spl]{postTime}[@spl]{message}\n")
 
@@ -39,3 +49,13 @@ def appGrid():
         return renderChat(request.form["username"])
     else:
         return renderChat()
+
+@app.route("/emoji", methods=('GET', 'POST'))
+def emojiSelector():
+    template = readFile("static/emoji.html")
+    contents = os.listdir("static/emotes/")
+    emojiList = ""
+    for file in contents:
+        emojiList = emojiList + f'<div class="emojiEntry"><img src="static/emotes/{file}"> <h2>:{file.removesuffix(".png").removeprefix("face-")}:</h2></div>\n'
+    template = template.replace("$emoji", emojiList)
+    return template
